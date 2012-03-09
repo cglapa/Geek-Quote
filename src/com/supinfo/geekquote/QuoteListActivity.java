bb39f713b8d7ff636cdf9e260f2b41809457f5ab
Cyril Glapa <cyril.glapa@keuse.fr>
2012-03-10 00:59:10 +0100

    Creating addQuote method

--- src/com/supinfo/geekquote/QuoteListActivity.java
+++ src/com/supinfo/geekquote/QuoteListActivity.java
@@ -8,12 +8,19 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class QuoteListActivity extends Activity {
-	private ArrayList<Quote> quotesArray;
+	private ArrayList<Quote> quotesArray = new ArrayList<Quote>();
 	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+    
+    public void addQuote(String strQuote) {
+    	Quote quote = new Quote();
+    	quote.setStrQuote(strQuote);
+    	
+    	quotesArray.add(quote);
     }
 }
