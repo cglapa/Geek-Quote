941aefc81d8d8d760f6e5e35180c94356435fee7
Cyril Glapa <cyril.glapa@keuse.fr>
2012-03-10 00:54:27 +0100

    Defining quotes ArrayList in the activity

--- src/com/supinfo/geekquote/QuoteListActivity.java
+++ src/com/supinfo/geekquote/QuoteListActivity.java
@@ -1,9 +1,15 @@
 package com.supinfo.geekquote;
 
+import java.util.ArrayList;
+
+import com.supinfo.geekquote.model.Quote;
+
 import android.app.Activity;
 import android.os.Bundle;
 
 public class QuoteListActivity extends Activity {
+	private ArrayList<Quote> quotesArray;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
