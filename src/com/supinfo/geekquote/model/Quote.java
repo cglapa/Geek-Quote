ef4eb2182672b439262d1a3738d14615e16a0d2d
Cyril Glapa <cyril.glapa@keuse.fr>
2012-03-10 00:52:57 +0100

    Creating Quote model

---
+++ src/com/supinfo/geekquote/model/Quote.java
@@ -0,0 +1,28 @@
+package com.supinfo.geekquote.model;
+
+import java.util.Date;
+
+public class Quote {
+	private String strQuote;
+	private int rating;
+	private Date creationDate;
+	
+	public String getStrQuote() {
+		return strQuote;
+	}
+	public void setStrQuote(String strQuote) {
+		this.strQuote = strQuote;
+	}
+	public int getRating() {
+		return rating;
+	}
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+	public Date getCreationDate() {
+		return creationDate;
+	}
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	} 
+}
