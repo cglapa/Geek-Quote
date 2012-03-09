0a522d4d31db20e10b3daadc7430f3fb3569b9b1
Cyril Glapa <cyril.glapa@keuse.fr>
2012-03-10 00:48:05 +0100

    Creating the project

---
+++ src/com/supinfo/geekquote/QuoteListActivity.java
@@ -0,0 +1,13 @@
+package com.supinfo.geekquote;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class QuoteListActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
