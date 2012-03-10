fef9d4aa618627b95577cfe31cca8df77426b62f
Cyril Glapa <cyril.glapa@keuse.fr>
2012-03-11 00:45:56 +0100

    Creating QuoteListAdapter

---
+++ src/com/supinfo/geekquote/adapter/QuoteListAdapter.java
@@ -0,0 +1,49 @@
+package com.supinfo.geekquote.adapter;
+
+import java.util.List;
+
+import com.supinfo.geekquote.model.Quote;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+public class QuoteListAdapter extends BaseAdapter {
+	private boolean isEven = false;
+	private List<Quote> quotes;
+	private Context context;
+	
+	public QuoteListAdapter(List<Quote> quotes, Context context) {
+		this.quotes = quotes;
+		this.context = context;
+	}
+
+	public int getCount() {
+		return quotes.size();
+	}
+
+	public Quote getItem(int position) {
+		return quotes.get(position);
+	}
+
+	public long getItemId(int position) {
+		return position;
+	}
+
+	public View getView(int position, View convertView, ViewGroup parent) {
+		TextView text = new TextView(context);
+
+        Quote item = getItem(position);
+        text.setText(item.getStrQuote());
+        
+        if(isEven)
+        	text.setBackgroundColor(Color.DKGRAY);
+        isEven = !isEven;
+
+        return text;
+	}
+
+}
