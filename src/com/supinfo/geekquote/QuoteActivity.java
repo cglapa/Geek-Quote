a1da064361ddc50db0913c82984a5b03f25b7ede
Cyril Glapa <cyril.glapa@keuse.fr>
2012-03-14 20:28:54 +0100

    Creating new QuoteActivity

---
+++ src/com/supinfo/geekquote/QuoteActivity.java
@@ -0,0 +1,14 @@
+package com.supinfo.geekquote;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class QuoteActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.quoteactivity);
+	}
+
+}
